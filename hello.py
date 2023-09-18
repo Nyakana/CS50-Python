d1939f21868f5c25776c7e39cc1c0f416dc9e7e1
Nyakana <nyakanaadam@gmail.com>
2023-09-18 13:11:11 +0300

    git

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
-name = input("What is your name?: ")
+#name = input("What is your name?: ")
 #age = input("How old are you?: ")
-name = name.strip().title()
-print("Hello " + name )    
+#name = name.strip().title()
+#print("Hello " + name )# 
+ 
+X = int(input("what`s X? "))
+Y = int(input("what`s Y? "))
+
+
+print(X + Y)
