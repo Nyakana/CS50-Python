df5096b84571d74775d22659b7c8eb6db28b0734
Nyakana <nyakanaadam@gmail.com>
2023-09-21 12:09:39 +0300

    new

--- hello.py
+++ hello.py
@@ -3,8 +3,16 @@
 #name = name.strip().title()
 #print("Hello " + name )# 
  
+#X = int(input("what`s X? "))
+#Y = int(input("what`s Y? "))
+
+
+#print(X + Y)
+
 X = int(input("what`s X? "))
 Y = int(input("what`s Y? "))
 
-
-print(X + Y)
+if X != Y:
+    print("X is not eqaul Y")
+else:
+    print("X is equal to Y")
